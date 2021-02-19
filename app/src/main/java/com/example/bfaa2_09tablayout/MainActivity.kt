c275040440002281e2db0a7a1c0aa4c60107a3d1
roynaldi19 <roynaldi19@gmail.com>
2021-02-19 19:41:27 +0700

    Initial commit

---
+++ app/src/main/java/com/example/bfaa2_09tablayout/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.bfaa2_09tablayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        view_pager.adapter = sectionsPagerAdapter
+        tabs.setupWithViewPager(view_pager)
+        supportActionBar?.elevation = 0f
+    }
+}
